8d8cf2777c21f3372c55d4131018a34ece9557af
brendanvanbreda <brendanvanbreda@users.noreply.github.com>
2017-10-18 12:12:58 +0200

    Update xVersion.h

--- src/xVersion.h
+++ src/xVersion.h
@@ -7,20 +7,4 @@
 
 #define LIBRARY_VERSION "1.0.1"
 
-#define CR0X_RED 14
-#define CR0X_GREEN 15
-#define CR0X_BLUE 16
-
-#define CW01_RED 12
-#define CW01_GREEN 13
-#define CW01_BLUE 5
-
-#define CC01_RED 5
-#define CC01_GREEN 6
-#define CC01_BLUE 7
-
-#define CW02_RED 25
-#define CW02_GREEN 26
-#define CW02_BLUE 27
-
 #endif
