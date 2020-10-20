6f3b2882eba84dc6b45f232d9e78efde6384a984
luqmaanb <lb@xinabox.cc>
2020-10-20 14:41:15 +0200

    Added macros for rgb

--- src/xCore.h
+++ src/xCore.h
@@ -1,10 +1,10 @@
 /********************************************************
- 	This is a library for the XinaBox Core Functions	
+ 	This is a library for the XinaBox Core Functions
 
- 	Core Functions include:								
- 	-	I2C Communication										
+ 	Core Functions include:
+ 	-	I2C Communication
  	-	Inter device communication
-	
+
 *********************************************************/
 
 #ifndef	xCore_h
@@ -16,6 +16,22 @@
 #include <Arduino.h>
 #include "xVersion.h"
 #include "Wire.h"
+
+#if not defined(CORE_RED) || defined(CORE_GREEN) ||defined(CORE_BLUE)
+#ifdef ESP8266
+#define CORE_RED 12
+#define CORE_GREEN 13
+#define CORE_BLUE 5
+#elif ESP32
+#define CORE_RED 25
+#define CORE_GREEN 26
+#define CORE_BLUE 27
+#elif ARDUINO_SAMD_ZERO
+#define CORE_RED 11
+#define CORE_GREEN 12
+#define CORE_BLUE 13
+#endif
+#endif
 
 
 /*********************************************************/
@@ -33,8 +49,8 @@ class xCoreClass{
 		uint8_t 	read8(byte device, byte reg);
 		uint16_t	read16(byte device, byte reg);
 		int16_t 	readS16(byte device,byte reg);
-		uint16_t  	read16_LE(byte device, byte reg); 
-		int16_t   	readS16_LE(byte device, byte reg); 
+		uint16_t  	read16_LE(byte device, byte reg);
+		int16_t   	readS16_LE(byte device, byte reg);
 		uint32_t 	read24(byte device, byte reg);
 		bool		ping(byte device);
 };
